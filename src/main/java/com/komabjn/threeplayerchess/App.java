3115591e169e664dff60685c91e3bd57cad025c3
Paweł Kowalewski <komabjn@gmail.com>
2022-11-20 23:45:39 +0100

    First version of chessboard renderer

--- src/main/java/com/komabjn/threeplayerchess/App.java
+++ src/main/java/com/komabjn/threeplayerchess/App.java
@@ -1,5 +1,9 @@
 package com.komabjn.threeplayerchess;
 
+import com.komabjn.threeplayerchess.rendering.ThreePlayerChessboardRendererFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
 /**
  *
  * @author Komabjn
@@ -8,5 +12,11 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        SwingUtilities.invokeLater(() -> {
+            ThreePlayerChessboardRendererFrame renderer = new ThreePlayerChessboardRendererFrame();
+            renderer.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            renderer.pack();
+            renderer.setVisible(true);
+        });
     }
 }
