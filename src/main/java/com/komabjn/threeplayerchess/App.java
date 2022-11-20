f45575256e06bed27d21e0ca4c2ab06f112df7d6
Paweł Kowalewski <komabjn@gmail.com>
2022-11-20 14:52:00 +0100

    Initial project

---
+++ src/main/java/com/komabjn/threeplayerchess/App.java
@@ -0,0 +1,12 @@
+package com.komabjn.threeplayerchess;
+
+/**
+ *
+ * @author Komabjn
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
